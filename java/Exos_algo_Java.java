74246f19935117ae302c0fd3155ce451608b862a
Aurélien Krieger <a.krieger.b@gmail.com>
2023-02-07 16:13:34 +0000

    Add files via upload

---
+++ java/Exos_algo_Java.java
@@ -0,0 +1,93 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Exos_algo_Java {
+    // 0.
+    // Write a function that displays "Hello World" in the console (use "System.out.println")
+
+    public static void ex0() {
+        System.out.println("Hello world");
+    }
+
+    // 1.
+    // Write a function that prompt the user for a number
+    // and then calculate the square value of that number 
+
+
+
+    // 2.
+    // Write a function that prompt the user for two numbers
+    // then exchange the values of the two numbers
+    // Example, if a = 2 and b = 5, the function displays a = 5 and b = 2
+
+
+    // 3.
+    // Write a function that takes as parameters:
+    // the prix excl. tax of an article, the number of articles and the VAT percentage
+    // then displays the total price incl. tax
+
+    // 4.
+    // Write a function that takes as parameters:
+    // two numbers
+    // then displays whether the product of these two numbers is positive, negative or equals zero
+    // Warning: we shall not calculate the product
+
+
+    // 5.
+    // Write a function that takes as parameter the age of a child
+    // then display their category:
+    // ”Poussin” if aged 6 to 7 years old
+    // ”Pupille” if aged 8 to 9 years old
+    // ”Minime” if aged 10 to 11 years old
+    // ”Cadet” if aged 12+ years old
+    // Use the key word "return" and display the result using "System.out.println" outside the function
+
+
+    // 6.
+    // Write a function that calculates a random number between 0 and 100
+    // The function prompts the user to guess the random number, until the answer is correct.
+    // The function should give the user hints whether the answer is greater or less than the random number
+    // Bonus : Display the score (how many guesses before getting it right)
+
+
+    // 7.
+    // Write a function that takes as parameter a number
+    // then calculates the cum of integrer numbers up to the input number
+    // Example, if we input 5, the function must calculate : 1 + 2 + 3 + 4 + 5 = 15
+    // NB : we shall only display the result, not the details of the calculation
+    // Use the key word "return" and display the result using "System.out.println" outside the function
+
+
+    // 8.
+    // Write a function that takes as parameter a number
+    // then calculates the factorial the input number
+    // Use the key word "return" and display the result using "System.out.println" outside the function
+
+
+    // 9.
+    // Write a function that takes as parameter the following table:
+    // [0, -3, 4, 9, 16, 25, -2]
+    // then calculates the maximum and minimum numbers of that table
+    // Use the key word "return" and display the couple (max et min) using "System.out.println" outside the function
+
+
+    // 10.
+    // Write function that takes as parameters two integrer numbers
+    // then returns a table containing 10 random integer number between the input numbers
+    // Use the key word "return" and display the table using "System.out.println" outside the function
+
+
+    // 11.
+    // Write function that takes as parameter a table of classroom marks
+    // (for this use the function of exercise 10.)
+    // then returns how many marks are above the class's average
+    // Use the key word "return" and display the result using "System.out.println" outside the function
+
+
+
+    public static void main(String[] args) {
+        ex0();
+    }
+
+}
+
